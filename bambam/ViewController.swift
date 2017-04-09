b260d711dc08818db9bbf91e6e443c0f89e56f85
chooblarin <choo.bla.rin@gmail.com>
2017-04-09 22:57:52 +0900

    Initial commit

---
+++ bambam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bambam
+//
+//  Created by Sota Hatakeyama on 2017/04/09.
+//  Copyright © 2017 Sota Hatakeyama. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
